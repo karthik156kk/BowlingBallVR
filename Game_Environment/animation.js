6efb975d1e67425fdffcda44edb42e06ca8b469e
Dhinesh-M-7 <dhini3322@gmail.com>
2024-03-18 16:29:45 +0530

    Config for animation, bowling ball and pins

--- Game_Environment/animation.js
+++ Game_Environment/animation.js
@@ -1,68 +1,55 @@
 import * as BABYLON from "@babylonjs/core";
 import { startMenuGUI } from "../Game_GUI/startMenuGUI";
+import config from "../config.json" assert {type: "json"};
 
-//Defining frame number and values for camera movement and rotation
-const movementFrames = [0, 1, 5, 6, 13, 14];
-const movementValues = [
-  new BABYLON.Vector3(0, 20, -120),
-  new BABYLON.Vector3(0, 10, -100),
-  new BABYLON.Vector3(0, 10, 60),
-  new BABYLON.Vector3(0, 30, 50),
-  new BABYLON.Vector3(0, 30, -100),
-  new BABYLON.Vector3(0, 25, -100),
-];
-
-const rotationFrames = [0, 5, 9, 13];
-const rotationValues = [0, 0, Math.PI, 2 * Math.PI];
-
-//creating a camera animation with the data above
+//creating a camera animation with the data from config 
 export const createAnimations = (camera, scene, game) => {
-  const frameRate = 30;
 
   const movement = new BABYLON.Animation(
     "movement",
     "position",
-    frameRate,
+    config.animation.frameRate,
     BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
     BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT,
     true
   );
   const movement_keys = [];
 
-  for (let frameId = 0; frameId < movementFrames.length; frameId++) {
+  for (const {frame, x, y, z} of config.animation.movementAnimationValues){
+    const position = new BABYLON.Vector3(x, y, z);
     movement_keys.push({
-      frame: movementFrames[frameId] * frameRate, // Multiply frame by frameRate
-      value: movementValues[frameId],
+      frame: frame * config.animation.frameRate, // Multiply frame by frameRate
+      value: position
     });
   }
-
   movement.setKeys(movement_keys);
 
   const rotation = new BABYLON.Animation(
     "rotate",
     "rotation.y",
-    frameRate,
+    config.animation.frameRate,
     BABYLON.Animation.ANIMATIONTYPE_FLOAT,
     BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT,
     true
   );
   const rotate_keys = [];
 
-  for (let frameId = 0; frameId < rotationFrames.length; frameId++) {
+  for (const {frame, yRotation} of config.animation.rotationAnimationValues){
     rotate_keys.push({
-      frame: rotationFrames[frameId] * frameRate, // Multiply frame by frameRate
-      value: rotationValues[frameId],
+      frame: frame * config.animation.frameRate, // Multiply frame by frameRate
+      value: yRotation
     });
   }
 
   rotation.setKeys(rotate_keys);
 
+
   //Starting the animation on the scene
   scene.beginDirectAnimation(
     camera,
     [movement, rotation],
-    0,
-    14 * frameRate,
+    config.animation.startFrame,
+    config.animation.endFrame * config.animation.frameRate,
     false,
     1,
     () => {
--- Game_Environment/bowlingBallAndPins.js
+++ Game_Environment/bowlingBallAndPins.js
@@ -1,15 +1,15 @@
 import * as BABYLON from "@babylonjs/core"
+import config from "../config.json" assert {type: "json"};
 
 export const createBowlingBall = (bowlingBallResult, scene) => {
     const bowling_ball = bowlingBallResult.meshes[1];
-    bowling_ball.scaling = new BABYLON.Vector3(1, 1, 1);
-    bowling_ball.position.y = 4;
-    bowling_ball.position.z = -62;
+    bowling_ball.scaling = new BABYLON.Vector3(config.ball.scaling[0], config.ball.scaling[1], config.ball.scaling[2]);
+    bowling_ball.position = new BABYLON.Vector3(config.ball.position[0], config.ball.position[1], config.ball.position[2]);
   
     const bowling_aggregator = new BABYLON.PhysicsAggregate(
       bowling_ball,
       BABYLON.PhysicsShapeType.SPHERE,
-      { mass: 1, restitution: 0.45, friction: 0.75}, scene
+      { mass: config.ball.mass, restitution: config.ball.restitution, friction: config.ball.friction}, scene
     )
     bowling_aggregator.body.disablePreStep = false;
     bowling_aggregator.body.setCollisionCallbackEnabled(true);
@@ -18,29 +18,20 @@ export const createBowlingBall = (bowlingBallResult, scene) => {
 
 export const createBowlingPins = (bowlingPinResult, scene) => {
     const bowlingPin = bowlingPinResult.meshes[1];
-    bowlingPin.scaling = new BABYLON.Vector3(1.5, 1.5, 1.5);
+    bowlingPin.scaling = new BABYLON.Vector3(config.pin.scaling[0], config.pin.scaling[1], config.pin.scaling[2]);
     bowlingPin.isVisible = false;
 
-    const pinPositions = [
-        new BABYLON.Vector3(-10, 0.5, 98),
-        new BABYLON.Vector3(-4, 0.5, 98),
-        new BABYLON.Vector3(4, 0.5, 98),
-        new BABYLON.Vector3(10, 0.5, 98),
+    const pinPositions = [];
 
-        new BABYLON.Vector3(0, 0.5, 94),
-        new BABYLON.Vector3(-7, 0.5, 94),
-        new BABYLON.Vector3(7, 0.5, 94),
-
-        new BABYLON.Vector3(-3.5, 0.5, 90),
-        new BABYLON.Vector3(3.5, 0.5, 90),
-
-        new BABYLON.Vector3(0, 0.5, 86),
-    ];
+    for (const {x, y, z} of config.pin.positions){
+      const position = new BABYLON.Vector3(x, y, z);
+      pinPositions.push(position);
+    }
 
     const setPins = pinPositions.map(function (position, idx) {
         const pin = new BABYLON.InstancedMesh("pin-" + idx, bowlingPin);
         pin.position = position;
-        const pinAggregate = new BABYLON.PhysicsAggregate(pin, BABYLON.PhysicsShapeType.CONVEX_HULL, { mass: 0.25, restitution: 0.1,friction:1.6 }, scene);
+        const pinAggregate = new BABYLON.PhysicsAggregate(pin, BABYLON.PhysicsShapeType.CONVEX_HULL, { mass: config.pin.mass, restitution: config.pin.restitution,friction:config.pin.friction }, scene);
         pinAggregate.body.setCollisionCallbackEnabled(true);
         return pin;
     });
